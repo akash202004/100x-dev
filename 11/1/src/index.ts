6ef506935a4c57cfa7911f3a8d6bd200e7d835e4
akash202004 <akashlaha48@gmail.com>
2024-09-22 21:11:40 +0530

    week 11.1 hono WIP

--- 11/1/src/index.ts
+++ 11/1/src/index.ts
@@ -3,3 +3,26 @@ export default {
 		return new Response('Hello World!');
 	},
 } satisfies ExportedHandler<Env>;
+
+// cloudflare environment for routing requests
+// cloudflare doesn't support express.js, so we need to use a different approach
+// it has a built-in fetch function that we can use to handle requests
+// but that was ugly so we use hono..
+
+// uglo cloudfare environment
+// export default {
+// 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+// 		console.log(request.body);
+// 		console.log(request.headers);
+
+// 		if (request.method === "GET") {
+// 			return Response.json({
+// 				message: "you sent a get request"
+// 			});
+// 		} else {
+// 			return Response.json({
+// 				message: "you did not send a get request"
+// 			});
+// 		}
+// 	},
+// };

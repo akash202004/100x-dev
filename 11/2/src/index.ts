0722d3bda135e3dbb3d248ec2b0a5b5507d3a145
akash202004 <akashlaha48@gmail.com>
2024-09-23 08:31:18 +0530

    week 11.1 complete

--- 11/2/src/index.ts
+++ 11/2/src/index.ts
@@ -1,9 +1,21 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+async function authMiddleware(c: any, next: any) {
+  if(c.req.header("Authorization")){
+    await next();
+  }else{
+    return c.text("Unauthorized", 401);
+  }
+}
 
-export default app
+app.get("/",authMiddleware, async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  return c.text("Hello Hono!");
+});
+
+export default app;

5284cd52b9dada0b6e4a5a2179acf539578e435c
akash202004 <akashlaha48@gmail.com>
2024-10-05 15:32:03 +0530

    deployed in cloudflare sucessuflly week 13 medium WIP

--- 13/Medium-blog/backend/src/index.ts
+++ 13/Medium-blog/backend/src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { decode, verify, sign } from "hono/jwt";
 import { userRouter } from "./routes/user";
 import { blogRouter } from "./routes/blog";
 
--- 13/Medium-blog/backend/src/routes/blog.ts
+++ 13/Medium-blog/backend/src/routes/blog.ts
@@ -1,41 +1,150 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
-import { decode, verify, sign } from "hono/jwt";
+import { verify } from "hono/jwt";
 
+// Create a new blog router
 export const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
+  Variables: {
+    authorId: string;
+  };
 }>();
 
+// Middleware to check if the user is logged in
 blogRouter.use("/*", async (c, next) => {
-  const header = c.req.header("Authorization");
-  if (!header) {
-    c.status(401);
-    return c.json({ message: "Unauthorized: No Authorization header" });
+  try {
+    const header = c.req.header("Authorization") || "";
+    if (!header) {
+      c.status(401);
+      return c.json({ message: "Unauthorized: No Authorization header" });
+    }
+
+    const token = header.split(" ")[1];
+    if (!token) {
+      c.status(401);
+      return c.json({ message: "Unauthorized: Token not provided" });
+    }
+    const response: any = await verify(token, c.env.JWT_SECRET);
+
+    if (response) {
+      c.set("authorId", response.id);
+      return next();
+    } else {
+      c.status(401);
+      return c.json({ message: "You are not logged in" });
+    }
+  } catch (error) {
+    c.status(500);
+    return c.json({ message: "Internal Server Error" });
   }
-  const token = header.split(" ")[1];
-  const response = await verify(token, c.env.JWT_SECRET);
-  if (response.id) {
-    return next();
-  } else {
-    c.status(401);
-    return c.json("Unauthorized");
+});
+
+// Create a new blog
+blogRouter.post("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const body = await c.req.json();
+    if (!body.title || !body.content) {
+      return c.json("Invalid title or content");
+    }
+    const authorId = c.get("authorId");
+    const blog = await prisma.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: authorId,
+      },
+    });
+
+    return c.json({
+      message: "Blog created successfully",
+      blog: blog,
+      id: blog.id,
+    });
+  } catch (error) {
+    c.status(500);
+    return c.json("Failed to create blog");
   }
 });
 
-blogRouter.post("/", (c) => {
-  return c.text("post blog");
+// Update a blog
+blogRouter.put("/", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const body = await c.req.json();
+    const blog = await prisma.post.update({
+      where: {
+        id: body.id,
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+
+    return c.json({
+      message: "Blog updated successfully",
+      blog: blog,
+      id: blog.id,
+    });
+  } catch (error) {
+    c.status(500);
+    return c.json("Failed to update blog");
+  }
 });
 
-blogRouter.put("/", (c) => {
-  return c.text("put blog");
+// Fetch a blog
+blogRouter.get("/get/:id", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const id = c.req.param("id");
+    const blog = await prisma.post.findFirst({
+      where: {
+        id: id.toString(),
+      },
+    });
+
+    if (blog) {
+      return c.json({
+        message: "Blog Fetched successfully",
+        blog: blog,
+      });
+    } else {
+      return c.json("Blog not found");
+    }
+  } catch (error) {
+    c.status(500);
+    return c.json("Failed to fetch blog");
+  }
 });
 
-blogRouter.get("/", (c) => {
-  return c.text("blog id");
-});
+// Fetch all blogs
+blogRouter.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-blogRouter.get("/bulk", (c) => {
-  return c.text("blog id");
+  try {
+    const blogs = await prisma.post.findMany();
+    return c.json({
+      message: "Blogs fetched successfully",
+      blogs: blogs,
+    });
+  } catch (error) {
+    c.status(500);
+    return c.json("Failed to fetch blog");
+  }
 });

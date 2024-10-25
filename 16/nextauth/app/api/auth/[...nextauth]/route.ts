7a0e6c586e0bb9d95f57254bdb83502fc791a9d8
akash202004 <akashlaha48@gmail.com>
2024-10-25 19:36:29 +0530

    week 16.2 offline next-auth WIP

---
+++ 16/nextauth/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Email",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Username" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials: any) {
+        return {
+          id: "user1",
+        };
+      },
+    }),
+  ],
+});
+
+export const GET = handler;
+export const POST = handler;

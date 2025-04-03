e64f5beabddf95e5d15a6465e1bc7f37a34b3cb3
akash202004 <akashlaha48@gmail.com>
2025-04-03 11:23:26 +0530

    week 24.3 mocking & deep mocking & ci/cd

--- 24/vitest-mocking/src/index.ts
+++ 24/vitest-mocking/src/index.ts
@@ -6,51 +6,60 @@ export const app = express();
 app.use(express.json());
 
 const sumInput = z.object({
-    a: z.number(),
-    b: z.number()
-})
-
-app.post("/sum", async (req, res) => {
-    const parsedResponse = sumInput.safeParse(req.body)
-    
-    if (!parsedResponse.success) {
-         res.status(411).json({
-            message: "Incorrect inputs"
-        })
-        return;
-    }
-
-    const answer = parsedResponse.data.a + parsedResponse.data.b;
-
-    await prismaClient.sum.create({
-        data: {
-            a: parsedResponse.data.a,
-            b: parsedResponse.data.b,
-            result: answer
-        }
-    })
-
-    res.json({
-        answer
-    })
+  a: z.number(),
+  b: z.number(),
 });
 
-app.get("/sum", (req, res) => {
-    const parsedResponse = sumInput.safeParse({
-        a: Number(req.headers["a"]),
-        b: Number(req.headers["b"])
-    })
-    
-    if (!parsedResponse.success) {
-         res.status(411).json({
-            message: "Incorrect inputs"
-        })
-        return
+app.post("/sum", async (req, res) => {
+  const parsedResponse = sumInput.safeParse(req.body);
+
+  if (!parsedResponse.success) {
+    res.status(411).json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  await prismaClient.sum.create({
+    data: {
+      a: parsedResponse.data.a,
+      b: parsedResponse.data.b,
+      result: answer,
+    },
+  });
+
+  res.json({
+    answer,
+  });
+});
+
+app.get("/sum", async(req, res) => {
+  const parsedResponse = sumInput.safeParse({
+    a: Number(req.headers["a"]),
+    b: Number(req.headers["b"]),
+  });
+
+  if (!parsedResponse.success) {
+    res.status(411).json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
+  const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+  const response = await prismaClient.sum.create({
+    data: {
+        a: parsedResponse.data.a,
+        b: parsedResponse.data.b,
+        result: answer
     }
+})
 
-    const answer = parsedResponse.data.a + parsedResponse.data.b;
-
-    res.json({
-        answer
-    })
+  res.json({
+    answer,
+    id: response.id
+  });
 });
--- 24/vitest-mocking/src/tests/index.test.ts
+++ 24/vitest-mocking/src/tests/index.test.ts
@@ -1,34 +1,61 @@
 import { describe, expect, it, vi } from "vitest";
 import request from "supertest";
 import { app } from "../index";
+import { prismaClient } from "../__mocks__/db";
+
+// deep mocking
+vi.mock("../db");
 
 // mocking
 // vi.mock("../db", () => ({
 //   prismaClient: { sum: { create: vi.fn() } },
 // }));
 
-// deep mocking
-vi.mock("../db");
-
 describe("POST /sum", () => {
+  // mocking a return value by putting hardcore value
   it("should return the sum of two numbers", async () => {
+    prismaClient.sum.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 2,
+      result: 3,
+    });
+
+    vi.spyOn(prismaClient.sum, "create");
+
     const res = await request(app).post("/sum").send({
       a: 1,
       b: 2,
     });
+
+    expect(prismaClient.sum.create).toHaveBeenCalledWith({
+      data: {
+        a: 1,
+        b: 2,
+        result: 3,
+      },
+    });
+
     expect(res.statusCode).toBe(200);
     expect(res.body.answer).toBe(3);
   });
+});
 
-  it("should return 411 if no inputs are provided", async () => {
-    const res = await request(app).post("/sum").send({});
-    expect(res.statusCode).toBe(411);
-    expect(res.body.message).toBe("Incorrect inputs");
-  });
+it("should return 411 if no inputs are provided", async () => {
+  const res = await request(app).post("/sum").send({});
+  expect(res.statusCode).toBe(411);
+  expect(res.body.message).toBe("Incorrect inputs");
 });
 
 describe("GET /sum", () => {
   it("should return the sum of two numbers", async () => {
+    prismaClient.sum.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 1,
+      result: 3,
+    });
+
     const res = await request(app)
       .get("/sum")
       .set({

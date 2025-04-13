f729c09fc1959754bdc5f0ef0ec14ff3a9f6dcbf
akash202004 <akashlaha48@gmail.com>
2025-04-13 10:40:13 +0530

    week 25 complete

---
+++ 25/2-cypress-test/src/cypress-test.js
@@ -0,0 +1,23 @@
+// cypress extension not shoiwng when i open the cypress window so i write the code here
+
+describe('Testing app', () => {
+    beforeEach(() => {
+      cy.visit('https://app.100xdevs.com')
+    })
+  
+    it('is able to log in', () => {
+      cy.contains('Login').should('exist')
+      cy.contains('Login').click()
+      cy.contains('Signin to your Account').should('exist', { timeout: 10000 })
+      cy.get('#email').type('harkirat.iitr@gmail.com');
+  
+      // Fill in the password field
+      cy.get('#password').type('123random');
+  
+      cy.get('button').eq(4).click()
+  
+      cy.contains('View Content').should("exist", {timeout: 10000})
+    })
+  
+  })
+  
